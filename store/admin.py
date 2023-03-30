c522b3721b8024f8541bbb446d9ce68eecad3a41
naheantarik <nahean.swe@gmail.com>
2023-03-30 14:58:36 +0600

    Initial commit

--- store/admin.py
+++ store/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models.product import Product
 from .models.category import Category
 
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
 # Register your models here.
 
-admin.site.register(Product)
+
+admin.site.register(Product, AdminProduct)
 admin.site.register(Category)
