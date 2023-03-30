8591e460b4ef6927dfb14f69c8aab9687e8408a4
naheantarik <nahean.swe@gmail.com>
2023-03-30 13:55:51 +0600

    Initial commit

--- store/models.py
+++ store/models.py
@@ -6,9 +6,16 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=60)
     price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
     description = models.CharField(max_length=200, default='', blank=True)
     image = models.ImageField(upload_to='Media/products/')
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
