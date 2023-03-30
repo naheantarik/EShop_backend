e9ce5dcd2795060f2a8bf458bbab7071d5cb2a5b
naheantarik <nahean.swe@gmail.com>
2023-03-30 13:24:56 +0600

    Initial commit

--- store/models/product.py
+++ store/models/product.py
@@ -5,4 +5,4 @@ class Product(models.Model):
     name = models.CharField(max_length=60)
     price = models.IntegerField(default=0)
     description = models.CharField(max_length=200, default='', blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='Media/products/')
