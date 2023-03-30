80c9a89ce8bbb7cf7bcad1de6b5fe174c166c0e0
naheantarik <nahean.swe@gmail.com>
2023-03-30 13:34:58 +0600

    Initial commit

--- store/models.py
+++ store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.IntegerField(default=0)
+    description = models.CharField(max_length=200, default='', blank=True)
+    image = models.ImageField(upload_to='Media/products/')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
--- store/models/product.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=60)
-    price = models.IntegerField(default=0)
-    description = models.CharField(max_length=200, default='', blank=True)
-    image = models.ImageField(upload_to='Media/products/')
