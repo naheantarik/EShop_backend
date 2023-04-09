0e9d6712225050d3b96e7fe9854e718f0f4c9b56
naheantarik <nahean.swe@gmail.com>
2023-04-10 03:25:31 +0600

    Initial commit

--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models.product import Product
+from .models.product import Product, Category
 
 # Create your views here.
 
 
 def index(request):
     prduct = Product.get_all_product()
+    category = Category.get_all_category()
 
-    return render(request, 'index.html', {'products': prduct})
+    return render(request, 'index.html', {'products': prduct}, {'categorys': category})
